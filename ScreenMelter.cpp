99b5723b4e55700ca24b7a4e392544f1216d9bad
fearleas <fearleasoff@gmail.com>
2017-01-05 20:00:48 +0100

    added ScreenMelter.cpp

---
+++ ScreenMelter.cpp
@@ -0,0 +1,27 @@
+// stadfx.h comes with visual studio
+#include "stdafx.h"
+#include <iostream>
+#include <Windows.h>
+#include <wingdi.h>
+
+int main() {
+	// If don't want the console use this, else just remove this line
+	FreeConsole();
+
+	// Get screen stats
+	HDC dcD = GetWindowDC(NULL);
+	int width = GetSystemMetrics(SM_CXSCREEN);
+	int height = GetSystemMetrics(SM_CYSCREEN);
+
+	// Seed random number generator
+	srand(GetTickCount());
+
+	while (true)
+	{
+		// Random x coordinate
+		int x = rand() % width;
+		BitBlt(dcD, x, 20, 50, height, dcD, x, 0, SRCCOPY);
+	}
+
+	return 0;
+}
